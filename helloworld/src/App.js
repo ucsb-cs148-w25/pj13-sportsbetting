21178095a27fda22229f7ffdd3f10c6ba2d8030c
Tommy <79723996+Tommygithubaccount123@users.noreply.github.com>
2025-01-11 00:01:49 -0800

    initial app

---
+++ helloworld/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+      <h1>Hello, World!</h1>
+      <h2>Thomas So</h2>
+      {/* github repo link below */}
+      <a href="https://github.com/ucsb-cs148-w25/pj13-sportsbetting">Github Repo</a>
+    </div>
+  );
+}
+
+export default App;
+
