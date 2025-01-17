51e7a4e4b90e9f99800b6928885a0a253f356025
Andy Jin <zhengyangjin@ucsb.edu>
2025-01-17 00:41:43 -0800

    lab1

---
+++ AndyJin - Lab1/cs148lab1/src/test/java/com/example/cs148lab1/Cs148lab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cs148lab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs148lab1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
