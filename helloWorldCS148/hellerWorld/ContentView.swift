817e93519f32988ce7bcd6755ced967183b0fb24
Ryan Vo <97767331+ryanvo504@users.noreply.github.com>
2025-01-17 12:10:41 -0800

    Add files via upload

---
+++ helloWorldCS148/hellerWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hellerWorld
+//
+//  Created by Ryan Vo on 10/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello World from Ryan Vo")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ helloWorldCS148/hellerWorld/hellerWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  hellerWorldApp.swift
+//  hellerWorld
+//
+//  Created by Ryan Vo on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct hellerWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
