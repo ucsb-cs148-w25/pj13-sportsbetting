ddfe369a59236ab0893d0c8943aaa0ef2a73c4a7
Andy Jin <zhengyangjin@ucsb.edu>
2025-01-30 09:46:26 -0800

    [FIX] Added port to solve CORS issue

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.BACKEND_SERVER_PORT || 5000;
 const HOST = process.env.BACKEND_SERVER_HOST || "http://localhost"; // Default if undefined
 
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:3001"], // Allow frontend URLs
+    origin: ["http://localhost:5173", "http://localhost:3001","http://localhost:3000"], // Allow frontend URLs
     methods: ["GET", "POST", "PUT", "DELETE"],
 }));
 
