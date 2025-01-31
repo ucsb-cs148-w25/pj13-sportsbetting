025be2e7d5e874c6feb77e2bfa57f90c3dfeec3f
Andy Jin <zhengyangjin@ucsb.edu>
2025-01-31 02:56:34 -0800

    [FEAT] correct frontend deployed URL

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 const HOST = process.env.BACKEND_SERVER_HOST || "http://localhost"; // Default if undefined
 
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:3001","http://localhost:3000","https://pj13-sportsbetting.onrender.com"], // Allow frontend URLs
+    origin: ["http://localhost:5173", "http://localhost:3001","http://localhost:3000","https://pj13-sportsbetting-1-frontend.onrender.com"], // Allow frontend URLs
     methods: ["GET", "POST", "PUT", "DELETE"],
 }));
 
