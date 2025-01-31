fa1628b1f3894f0f8b087626e6fa47d5802fa76c
Bryce <brycewang@ucsb.edu>
2025-01-31 10:45:11 -0800

    fix

--- betbuddies/src/firebase.js
+++ betbuddies/src/firebase.js
@@ -1,19 +1,3 @@
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-
-// const firebaseConfig = {
-//     apiKey: "AIzaSyC_rFR8mf3U7LzDP9Q8eNjq83c2YXLi424",
-//     authDomain: "betbuddies-3f7c6.firebaseapp.com",
-//     projectId: "betbuddies-3f7c6",
-//     storageBucket: "betbuddies-3f7c6.firebasestorage.app",
-//     messagingSenderId: "241217604196",
-//     appId: "1:241217604196:web:73c2985de6d9c0b4f92d71",
-//     measurementId: "G-0NQD0VN2C2"
-// };
-
-// const app = initializeApp(firebaseConfig);
-// export const auth = getAuth(app);
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
