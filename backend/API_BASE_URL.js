43f37d69a6e3ee6c23bdbd9fce9cc6a326b5bbaa
Tommy <79723996+Tommygithubaccount123@users.noreply.github.com>
2025-02-01 17:07:59 -0800

    stupid

--- backend/API_BASE_URL.js
+++ backend/API_BASE_URL.js
@@ -1,6 +1,12 @@
 // Load environment variables from the .env file
 import dotenv from 'dotenv';
-dotenv.config();
+import path from 'path';
+import { fileURLToPath } from 'url';
+// Get the directory name of the current module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 const BACKEND_SERVER_PORT = process.env.PORT || 5000;
 const BACKEND_SERVER_HOST = process.env.BACKEND_SERVER_HOST || 'http://localhost';
