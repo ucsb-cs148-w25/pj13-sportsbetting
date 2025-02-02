18a52fe568754ad251e5acbb29194af43ed0519d
Tommy <79723996+Tommygithubaccount123@users.noreply.github.com>
2025-02-02 14:59:58 -0800

    concurrent jobs

--- backend/scripts/update_bets.js
+++ backend/scripts/update_bets.js
@@ -78,13 +78,17 @@ async function parse_api_response(scores) {
 async function script() {
     const scores = await fetchScores();
     const new_bet_winner_pairs = await parse_api_response(scores);
-    let total_updated = 0;
-    for (const pair of new_bet_winner_pairs) {
-        total_updated += await update_winner(pair.bet_id, pair.winner);
-    }
+
     console.log("Number of fetched bets: ", new_bet_winner_pairs.length);
     console.log(new_bet_winner_pairs);
-    console.log('Updated ', total_updated, ' bets');
+
+    const results = await Promise.allSettled(
+        new_bet_winner_pairs.map(pair => update_winner(pair.bet_id, pair.winner))
+    );
+
+    const total_updated = results.filter(result => result.status === "fulfilled" && result.value === 1).length;
+
+    console.log('Updated', total_updated, 'bets');
 }
 
 script();
